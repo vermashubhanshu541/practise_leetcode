eae6fbba0e16e89da72e74a88c8e9bb3acda4de8
Java codes <vermashubhanshu541@gmail.com>
2023-06-23 17:47:24 +0530

    java codes

--- src/stockprofit.java
+++ src/stockprofit.java
@@ -1,64 +1,29 @@
 import java.util.*;
 public class stockprofit {
-    public static void maxprofit(int[] prices,int size)
-    {
-        int maxprofit=0;
-        for(int i=1;i<size;i++)
-        {
-            if(prices[i]>prices[i-1])
-                maxprofit+=prices[i]-prices[i-1];
-        }
-        System.out.println(maxprofit);
-    }
-   static public void addStrings(String num1, String num2) {
-        String[] arr1=num1.split("");
-        String[] arr2=num2.split("");
-        int l1=arr1.length;
-        int l2=arr2.length;
-        int carry=0;
-        Stack<Integer> st=new Stack<>();
-        while(l1>0 && l2>0){
-            int sum=carry+Integer.parseInt(arr1[l1-1])+Integer.parseInt(arr2[l2-1]);
-            carry=sum/10;
-            st.add(sum%10);
-            l1--;
-            l2--;
-        }
-       System.out.println(st);
-        int maxLen=Math.max(l1,l2);
-        int flag=l1>l2?1:0;
-        while(maxLen>0 && carry>=0){
-            if(flag==1){
-                int a=Integer.parseInt(arr1[maxLen-1])+carry;
-                st.add(a%10);
-                carry=a/10;
-            }else if(flag==0){
-                int b=Integer.parseInt(arr2[maxLen-1])+carry;
-                st.add(b%10);
-                carry=b/10;
-            }
-            maxLen--;
-        }
-        if(carry!=0)
-            st.add(1);
-        String s="";
-        while(!st.isEmpty()){
-            s+=st.pop();
-        }
-       System.out.println(s);
+    public static int maxProfit(int[] prices, int fee) {
+        int buy = Integer.MIN_VALUE;
+        int sell = 0;
 
+        for (int price : prices) {
+            buy = Math.max(buy, sell - price);
+            sell = Math.max(sell, buy + price - fee);
+        }
 
+        return sell;
     }
     public static void main(String[] args)
     {
-//        Scanner sc=new Scanner(System.in);
-//        int len=sc.nextInt();
-//        int[] arr=new int[len];
-//        for(int i=0;i<len;i++)
-//        {
-//            arr[i]=sc.nextInt();
-//        }
-//        maxprofit(arr,len);
-        addStrings("3876620623801494171","6529364523802684779");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter length of the array:");
+        int len=sc.nextInt();
+        System.out.println("Enter elements in the array:");
+        int[] prices=new int[len];
+        for(int i=0;i<len;i++)
+        {
+           prices[i]=sc.nextInt();
+        }
+        System.out.println("Enter transaction fees:");
+        int fee=sc.nextInt();
+        System.out.println(maxProfit(prices,fee));
     }
 }
